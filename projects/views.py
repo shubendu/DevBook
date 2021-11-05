cbf243d23be38eb63e0b24004f518ba4a3ca9287
shubendu <biswasshubendu4@gmail.com>
2021-11-06 00:09:32 +0530

    done crud operations

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from . models import Project
+from .models import Project
+from .forms import ProjectForm
 
 
 
@@ -15,5 +16,31 @@ def project(request,pk):
     return render(request,'projects/single-project.html',{'project':projectObj,'tags':tags})
 
 def createProject(request):
-    context= {}
-    return render(request,'projects/project-form.html',{'context':context})
+    form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context= {'form':form}
+    return render(request,'projects/project-form.html',context)
+
+def updateProject(request,pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == "POST":
+        form = ProjectForm(request.POST,instance=project) #resquest.post will modify project with pk
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context= {'form':form}
+    return render(request,'projects/project-form.html',context)
+
+
+def deleteProject(request,pk):
+    project = Project.objects.get(id=pk)
+    if request.method == "POST":
+        project.delete()
+        return redirect('projects')
+    context={'object':projects}
+    return render(request,'projects/delete-template.html',context)
