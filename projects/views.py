3bce0cdc64bf3fbc21b48eb65ee7f394ab4f32c7
Shubendu Biswas <biswasshubendu4@gmail.com>
2021-11-09 11:43:10 +0530

    till developer=FALSE

--- projects/views.py
+++ projects/views.py
@@ -18,7 +18,7 @@ def project(request,pk):
 def createProject(request):
     form = ProjectForm()
     if request.method == "POST":
-        form = ProjectForm(request.POST)
+        form = ProjectForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
             return redirect('projects')
@@ -29,7 +29,7 @@ def updateProject(request,pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == "POST":
-        form = ProjectForm(request.POST,instance=project) #resquest.post will modify project with pk
+        form = ProjectForm(request.POST,request.FILES,instance=project) #resquest.post will modify project with pk
         if form.is_valid():
             form.save()
             return redirect('projects')
