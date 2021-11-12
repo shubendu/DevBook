3057de31a725cbb79c2fbe90f5b0efde3b4b47f8
Shubendu Biswas <biswasshubendu4@gmail.com>
2021-11-12 16:43:17 +0530

    add render 48

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('',views.profiles, name="profiles")
+    path('',views.profiles, name="profiles"),
+    path('user-profile/<str:pk>', views.userProfile, name="user-profile")
 ]
--- users/views.py
+++ users/views.py
@@ -5,3 +5,9 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles':profiles}
     return render(request, 'users/profiles.html',context)
+
+
+def userProfile(request,pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile': profile}
+    return render(request, 'users/user-profile.html',context)
