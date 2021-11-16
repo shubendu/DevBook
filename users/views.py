263f1fd67fbcc1683f2e340eb56fe4c45d89d583
Shubendu Biswas <biswasshubendu4@gmail.com>
2021-11-16 12:32:10 +0530

    done till rendering

--- users/views.py
+++ users/views.py
@@ -10,8 +10,8 @@ def profiles(request):
 def userProfile(request,pk):
     profile = Profile.objects.get(id=pk)
 
-    # topSkills = profile.skill_set.exclude(description)
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
 
-
-    context = {'profile': profile}
+    context = {'profile': profile,'topSkills':topSkills,'otherSkills':otherSkills}
     return render(request, 'users/user-profile.html',context)
